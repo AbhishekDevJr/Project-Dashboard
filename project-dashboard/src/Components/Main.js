6313076cdad1f73f6ff4aef1ed4ce0b90400e096
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-01-24 14:21:42 +0530

    Feat-UI Main and Card components completed, adding projects data.

--- project-dashboard/src/Components/Main.js
+++ project-dashboard/src/Components/Main.js
@@ -8,6 +8,13 @@ class Main extends Component {
     }
 
     render(){
+        let dummyText = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur consectetur eveniet natus minus molestias impedit, nobis recusandae reiciendis delectus doloremque, totam ullam in aliquam adipisci.';
+
+        let projectDiscription1 = "This project is a React Application. I have built a clone of 'Razor.com' using Tech like HTML5, CSS3, JS, React. Concepts like multiple UI Components, props, JSX, many css concepts like Flexbox and html concepts were covered.";
+
+        let projectDiscription2 = "This is a JavaScript based project. I have built an online Drawing Tool using HTML5, CSS3 and JavaScript. Concepts like DOM Manipulation, Event Handling and Event Listeners along with many CSS Properties like Pseudo State were covered.";
+        
+
         return(
             <div className = "main">
                 <main>
@@ -17,9 +24,12 @@ class Main extends Component {
                     </div>
 
                     <div className = "project-row1">
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
+                        <ProjectCard projectUrl1 = "https://razorbyabhishek.netlify.app/" gitHubUrl1 = "https://github.com/AbhishekDevJr/Razer-Clone-React" pName1 = "Razor WebSite Clone" content1 = {"Razor Clone Discription." + dummyText} projectUrl2 = "https://etch-a-sketch-by-abhishek.netlify.app/" gitHubUrl2 = "https://github.com/AbhishekDevJr/Etch-A-Sketch" pName2 = "Etch-A-Sketch Project" content2 = {"EtchASketchProject discription." + dummyText} content3 = {"Announcement discription." + dummyText}
+                        />
+                        <ProjectCard projectUrl1 = "https://spacexbyabhishek.netlify.app/" gitHubUrl1 = "https://github.com/AbhishekDevJr/spaceX-Clone" pName1 = "SpaceX Website Clone" content1 = {"SpaceX Clone discription" + dummyText} projectUrl2 = "https://odinformbyabhishek.netlify.app/" gitHubUrl2 = "https://github.com/AbhishekDevJr/TOP-FormProject" pName2 = "Form Validation Project" content2 = {"Top Form discription" + dummyText} content3 = {"Announcement discription" + dummyText}
+                        />
+                        <ProjectCard projectUrl1 = "https://abhishekdevjr.github.io/TOP-LandingPage/" gitHubUrl1 = "https://github.com/AbhishekDevJr/TOP-LandingPage" pName1 = "Landing Page Project" content1 = {"Top Landing discription" + dummyText} projectUrl2 = "https://nvidiabyabhishek.netlify.app/" gitHubUrl2 = "https://github.com/AbhishekDevJr/nvidiaClone" pName2 = "Nvidia Website Clone" content2 = {"Nvidia Discription" + dummyText} content3 = {"Announcement discription" + dummyText}
+                        />
                     </div>
                 </main>
             </div>
--- project-dashboard/src/Components/ProjectCard.js
+++ project-dashboard/src/Components/ProjectCard.js
@@ -10,32 +10,26 @@ class ProjectCard extends Component {
         return(
             <div className = "card-parent">
             <div className = "card">
-                <p>Project Name-1</p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ipsam maiores saepe beatae qui, necessitatibus dolores voluptatibus similique illo repellat dicta! Ipsum dicta nulla mollitia.
-                </p>
+                <strong><p>{this.props.pName1}</p></strong>
+                <p>{this.props.content1}</p>
                 <div className = "card-links">
-                    <a href = "/"><img src = "icons8-christmas-star-50.png" alt = "Star Icon"/></a>
-                    <a href = "/"><img src = "icons8-ophthalmology-30.png" alt = "View Icon"/></a>
+                    <a href = {this.props.gitHubUrl1} target = "_blank"><img src = "icons8-christmas-star-50.png" alt = "Star Icon"/></a>
+                    <a href = {this.props.projectUrl1} target = "_blank"><img src = "icons8-ophthalmology-30.png" alt = "View Icon"/></a>
                 </div>
             </div>
 
             <div className = "card">
-                <p>Project Name-1</p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ipsam maiores saepe beatae qui, necessitatibus dolores voluptatibus similique illo repellat dicta! Ipsum dicta nulla mollitia.
-                </p>
+            <strong><p>{this.props.pName2}</p></strong>
+                <p>{this.props.content2}</p>
                 <div className = "card-links">
-                    <a href = "/"><img src = "icons8-christmas-star-50.png" alt = "Star Icon"/></a>
-                    <a href = "/"><img src = "icons8-ophthalmology-30.png" alt = "View Icon"/></a>
+                    <a href = {this.props.gitHubUrl2}><img src = "icons8-christmas-star-50.png" alt = "Star Icon"/></a>
+                    <a href = {this.props.projectUrl2}><img src = "icons8-ophthalmology-30.png" alt = "View Icon"/></a>
                 </div>
             </div>
 
             <div className = "card">
-                <p>Project Name-1</p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ipsam maiores saepe beatae qui, necessitatibus dolores voluptatibus similique illo repellat dicta! Ipsum dicta nulla mollitia.
-                </p>
+            <strong><p>Project Name-1</p></strong>
+                <p>{this.props.content3}</p>
                 <div className = "card-links">
                     <a href = "/"><img src = "icons8-christmas-star-50.png" alt = "Star Icon"/></a>
                     <a href = "/"><img src = "icons8-ophthalmology-30.png" alt = "View Icon"/></a>
